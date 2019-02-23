f4855335bde463575ac26f9222fb577fb0792598
ofirl <ofirl24@gmail.com>
2019-02-23 10:09:45 +0200

    testing + start of switch component

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,1 +1,21 @@
-// Create switch
+import React, { Component } from 'react';
+
+class Switch extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            checked: 'profile'
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                switch
+            </div>
+        );
+    }
+}
+
+export default Switch;
